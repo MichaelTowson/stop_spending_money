0844339e4a8490ac1074edc6533ddfd72ed95552
stsnicky <stsnicky@gmail.com>
2020-12-21 22:37:24 -0800

    done index register page

--- ssm_application/views.py
+++ ssm_application/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 
+def index(request):
+    return render(request, "index.html")
+
 def goals(request):
     return render(request, "goals.html")
