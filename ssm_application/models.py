b99c408da6e5d566b253a57cff6d24db9585e889
Michael Towson <MTowson@gmail.com>
2020-12-21 17:48:36 -0800

    Reduced total number of html pages and re-named to match wireframes. Wrote out our models.

--- ssm_application/models.py
+++ ssm_application/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import bcrypt, re
 
-# Create your models here.
+class Manager(models.Manager):
+    def registerUser_valiadtor(self, postData):
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=80)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = Manager()
+    #goals = a list of goals that the user has made.
+
+class Goal(models.Model):
+    user = models.ForeignKey(User, related_name = "goals")
+    category = models.CharField(max_length=30)
+    balance = models.FloatField()
+    start_date = models.DateField()
+    #transactions - a list of transactions underneath each goal.
+
+class Transaction(models.Model):
+    goal = models.ForeignKey(Goal, related_name = "transactions") 
+    date = models.DateField()
+    amount = models.FloatField()
+    description = models.CharField(max_length = 50)
+    planned = models.CharField(max_length = 3)
+        #OPTIONS: (1) Yes, (2) No
+    happiness = models.CharField(max_length = 20)
+        #OPTIONS: (1) Very Happy, (2) Briefly Happy, (3) The Same, (4) Less Happy/Regret
+    
--- stop_spending_money/settings.py
+++ stop_spending_money/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ssm_application',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
