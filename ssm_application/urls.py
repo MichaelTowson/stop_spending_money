7fb1dcec192fa55659a2798910d428bcaeaa52df
Michael Towson <MTowson@gmail.com>
2020-12-23 00:39:19 -0800

    Styled login and register, moved style from login and register into CSS files, finished "add start date" functionality on goals

--- ssm_application/urls.py
+++ ssm_application/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('reg_user', views.register_user),
     path('log_in', views.log_in),
     path('goals/add_goal',views.add_goal),
+    path('goals/add_start_date',views.add_start_date),
     path('goals/delete_goal/<int:id>', views.delete_goal)
 ]
